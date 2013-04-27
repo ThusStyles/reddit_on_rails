3b145b1894cb41a4676ba36a5a3096afcbe72e18
Theo Styles <thus_styles@msn.com>
2013-04-27 21:07:40 +0100

    Imrpoved top links and recent links pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 	def index
-		@links = Link.order('upvotes_count desc, created_at desc').paginate(page: params[:page], per_page: 8)
+		@toplinks = Link.where('upvotes_count >= ?', '1')
+		@links = @toplinks.order('upvotes_count desc, created_at desc').paginate(page: params[:page], per_page: 8)
 	end
 
 	def recent

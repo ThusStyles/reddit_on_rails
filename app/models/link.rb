a63d9a578071e5176580579d82cca705cc861f0c
Theo Styles <thus_styles@msn.com>
2013-04-29 00:18:03 +0100

    Better url input

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,8 +6,14 @@ class Link < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   belongs_to :subreddit
 
+  before_validation :url_to_full
+
   validates :subreddit_id, presence: true
   validates_presence_of :message, if: lambda {self.type_of_link == "message"}
   validates :title, presence: true
-  validates :url, allow_blank: true, format: { with: /^https?:\/\/([a-zA-Z0-9?\/._-]+)(\.[a-zA-Z0-9\/#]{2,})/ }
+  validates :url, presence: true, format: { with: /^https?:\/\/([a-zA-Z0-9?\/._-]+)(\.[a-zA-Z0-9\/#]{2,})/ }
+
+  def url_to_full
+  	self.url = self.url.gsub(/^[w(3).]+/, "http://www.")
+  end
 end

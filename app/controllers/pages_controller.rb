e9d71417b6018ddccfe75d8c104cbdec3aba087a
Theo Styles <thus_styles@msn.com>
2013-04-29 16:25:33 +0100

    Changed since time for top links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 	def index
-		@toplinks = Link.since(Time.now - 48.hours)
+		@toplinks = Link.since(Time.now - 96.hours)
 		@toplinks2 = @toplinks.where('upvotes_count >= ?', '1')
 		@links = @toplinks2.order('upvotes_count desc, created_at desc').paginate(page: params[:page], per_page: 10)
 	end
